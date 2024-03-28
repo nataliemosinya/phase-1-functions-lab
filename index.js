4b82cdfc517c91054500ff6249667a6ee656ce74
natalie mosinya <natalie.mosinya@student.moringaschool.com>
2024-03-28 11:22:59 +0300

    Adds index.html

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42){
+        return (blocks - 42);
+        } else {
+        return (42 - blocks);
+    
+    }
+}
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet)*264
+}
+
+function distanceTravelledInFeet(start,destination) {
+    if (start > destination) {
+    return ( start - destination)*264
+    } else {
+    return ( destination - start)*264
+    }
+
+}
+
+
+function calculatesFarePrice(start, destination){
+let distance = distanceTravelledInFeet (start, destination)
+
+if ( distance < 400 ){
+    return 0
+}else if (distance > 400 && distance <= 2000){
+    return (distance - 400)*0.02
+}else if (distance > 2000 && distance <= 2500){
+    return 25
+}else{
+ return 'cannot travel that far'
+}
+} 
